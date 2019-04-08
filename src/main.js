7988d59f4560e28196b3801dcd391c4d6c16fd42
Adam <adam.rysyk@vibesmg.com>
2019-04-08 10:30:43 -0400

    add react root to main.js and index.html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 require('babel-runtime/regenerator');
+require('babel-register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
+require('./images/cat.jpg');
 require('./index.html');
+require('./app');

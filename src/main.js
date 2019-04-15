6317aad1d08b41cfd9239f101ab14e6e0e2f33f6
Adam <adam.rysyk@vibesmg.com>
2019-04-15 10:32:32 -0400

    change to a photo of Mochi

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ require('babel-runtime/regenerator');
 require('babel-register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
-require('./images/cat.jpg');
+require('./images/mochi.jpg');
 require('./index.html');
 require('./app');

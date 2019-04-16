5fc592f2168f78bf128d765225b6aa30e45973fe
Adam <adam.rysyk@vibesmg.com>
2019-04-16 10:17:58 -0400

    add typescript

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,7 +4,8 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: ['./src/main.js']
+        main: ['./src/main.js'],
+        ts: ['./src/index.ts']
     },
     mode: 'development',
     output: {
@@ -26,6 +27,12 @@ module.exports = {
             test: /\.js$/,
             use: [{
                 loader: 'babel-loader'
+            }],
+            exclude: /node_modules/
+        }, {
+            test: /\.ts$/,
+            use: [{
+                loader: 'awesome-typescript-loader'
             }],
             exclude: /node_modules/
         }, {
